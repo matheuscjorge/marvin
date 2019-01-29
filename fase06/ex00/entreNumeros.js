4a0fb3492966fa411a8e986eff4f58585bbba81a
matheuscjorge <44076081+matheuscjorge@users.noreply.github.com>
2019-01-29 10:30:38 -0200

    Update entreNumeros.js

--- fase06/ex00/entreNumeros.js
+++ fase06/ex00/entreNumeros.js
@@ -1,12 +1,3 @@
-/*function entreNumeros (min,max) {
-    Math.floor(min);
-    Math.ceil(max);
-    for (i = min; i <= max; i++){
-        console.log (i);
-    }
-}    
-entreNumeros (2,8);
-*/
 function entreNumeros (min,max) {
     var i = min
     while (i <= max){
