071ef401ce8e24a7af0f525b4a44929054f3da6d
matheuscjorge <44076081+matheuscjorge@users.noreply.github.com>
2019-02-06 13:58:24 -0200

    repetir item em str

--- fase08/ex06/bordas.js
+++ fase08/ex06/bordas.js
@@ -5,13 +5,21 @@ function bordas (h, v) {
     var B = "B"
     var x = "0"
     while (i < v) {
-        if (i < 1) 
-            console.log ("A" + B.repeat(h - 2) + "C")
-        else if (i < v - 1)
-            console.log ("B" + x.repeat(h - 2) + "B")
-        else
-            console.log ("C" + B.repeat(h - 2) + "A")
-        
+        if (h <= 1) {
+            if (i < 1)
+                console.log ("A")
+            else if (i < v - 1)
+                console.log ("B")
+            else
+                console.log ("C")
+        }else{
+            if (i < 1) 
+                console.log ("A" + B.repeat(h - 2) + "C")
+            else if (i < v - 1)
+                console.log ("B" + x.repeat(h - 2) + "B")
+            else
+                console.log ("C" + B.repeat(h - 2) + "A")
+        }
         i++;
     }
 }
